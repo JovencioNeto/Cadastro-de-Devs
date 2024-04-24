c9c10051f4eeda3f30900d115410e6caab2a9ba7
jovencio neto <151071159+JovencioNeto@users.noreply.github.com>
2024-04-24 14:52:14 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const name = document.getElementById('name').value
+const button = document.getElementById('button')
+
+button.addEventListener('click', () =>{
+    const label = document.createElement('label')
+    label.innerHTML = 'Nome das tecnologias'
+    document.createElement('br')
+    const tecnologias = document.createElement('input')
+    tecnologias.type = 'radio'
+
+    const time = document.createElement('button')
+
+    const remove = document.createElement('button')
+    
+})
